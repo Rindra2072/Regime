9c44bc627adfe966979045f07fc791035dbf41a1
Mamison Rajaoharisaona <narymams@gmail.com>
2023-07-10 11:44:18 +0300

    Commit
    Shema

--- shema.sql
+++ shema.sql
@@ -3,15 +3,53 @@ use regime;
 CREATE table Picture(
     id int PRIMARY KEY AUTO_INCREMENT,
     pic1 VARCHAR(255) NOT NULL,
-    pic2 VARCHAR(255) ,
-    pic3 VARCHAR(255) ,
+    pic2 VARCHAR(255),
+    pic3 VARCHAR(255),
     pic4 VARCHAR(255)
 );
+
+CREATE TABLE Objective(
+	id int PRIMARY KEY AUTO_INCREMENT,
+    objective VARCHAR(50) NOT NULL,
+	description VARCHAR(255)
+);
+
+INSERT INTO Objective VALUE(null,'Augmenter');
+INSERT INTO Objective VALUE(null,'Diminuer');
+
+
+
+CREATE TABLE Regime(
+	id INT  PRIMARY KEY AUTO_INCREMENT,
+	regime VARCHAR(50) NOT NULL,
+	id_picture REFERENCES Picture (id),
+	description VARCHAR(255)
+	id_Objective INT REFERENCES Objective (id),
+	weight DOUBLE NOT  NULL,
+	-- 7 days
+	price DOUBLE NOT  NULL
+);
+
+
+CREATE TABLE Regime_User(
+	id_User INT REFERENCES User(id),
+	id_Regime INT REFERENCES Regime (id),
+	objective_weight DOUBLE NOT  NULL,
+	duration INT --number of days
+);
+
+
+
 -- User
 create Table User(
     id int PRIMARY KEY AUTO_INCREMENT,
     user VARCHAR(50) NOT NULL,
     birthday DATE NOT NULL,
+	gender int NOT NULL,
+	size DOUBLE NOT  NULL,
+	weight DOUBLE NOT  NULL,
+	address VARCHAR(50) NOT  NULL,
+	contact VARCHAR(50) NOT  NULL,
     email VARCHAR(50) NOT NULL,
     key_password VARCHAR(50) NOT NULL,
     user_status int NOT NULL
@@ -19,13 +57,6 @@ create Table User(
 );
 -- User not confirmed
 
-create Table User_not_confirmed(
-    id int PRIMARY KEY AUTO_INCREMENT,
-    user VARCHAR(50) NOT NULL,
-    birthday DATE NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    key_password VARCHAR(50) NOT NULL,
-    user_status int NOT NULL DEFAULT 1
-);
+
 
 INSERT INTO User VALUES( null ,'tes',now(),'h@gmail.com','123',1);
